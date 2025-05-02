c7d5f06bfb99cd0829736998b4c227460251b5ac
Manoj <113231045+manoj-mj-2000@users.noreply.github.com>
2025-05-02 13:51:26 +0530

    Create DynamicArray.java
    An Implementation of Dynamic array

---
+++ DynamicArray.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+public class DynamicArray{
+		int startCapacity = 2;
+		int arr[];
+		int size, capacity;
+		int pos=0;
+
+		public DynamicArray() {
+			arr = new int[startCapacity];
+			capacity=startCapacity; 
+			size=0;
+		}
+		public int get(int index) {
+			return arr[index];
+		}
+		public void write(int val) {
+			if(size>=capacity) {
+				doubleCapacity();
+			}
+			arr[size++] = val;
+		}
+		private void doubleCapacity() {
+			capacity *= 2;
+			arr = Arrays.copyOf(arr, capacity);
+		}
+		public void update(int index, int val) {
+			arr[index] = val;
+		}
+		
+		public void insert(int index, int val) {
+			if(index>arr.length-1) {
+				throw new IndexOutOfBoundsException();
+			}
+			int x=0,y=0;
+			x = arr[index];
+			for(int i=index;i<arr.length-1;i++) {
+				y = arr[i+1];
+				arr[i+1] = x;
+				x = y;
+			}
+
+			arr[index]=val;
+			return;
+		}
+		
+		public int delete(int index) {
+			if(index > arr.length) {
+				throw new IndexOutOfBoundsException();
+			}
+			int val = arr[index];
+			int n = arr.length;
+			
+			for(int i=index; i<n; i++) {
+				arr[index] = arr[index+1];
+			}
+			pos--;
+			return val;
+		}
+		
+		public void arrToString() {
+			int n=arr.length;
+			for(int i=0;i<n; i++) {
+				System.out.print(arr[i]+" ");
+			}
+			System.out.println();
+		}
+	}
