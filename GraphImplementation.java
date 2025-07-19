ab627ac95bb06e988b5f8b6387abe25c038a5421
Manoj <113231045+manoj-mj-2000@users.noreply.github.com>
2025-07-19 22:52:50 +0530

    Graph Data Structure
    Graph data structure creation and printing using Adjacency list

---
+++ GraphImplementation.java
@@ -0,0 +1,56 @@
+//$Id$
+package com.dsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphImplementation {
+
+	public static void main(String[] args) {
+		// Create and work with Graph DataStructure
+		
+		Graph graph = new Graph();
+		
+		for(int i=0;i<5;i++) {
+			graph.insertElement(i);
+		}
+		
+		graph.insertEdge(0, 1);
+		graph.insertEdge(2, 3);
+		graph.insertEdge(1, 2);
+		graph.insertEdge(3, 4);
+		graph.insertEdge(0, 4);
+		graph.insertEdge(1, 4);
+		
+		graph.printAllConnections();
+
+	}
+	
+}
+
+class Graph{
+	
+	List<List<Integer>> graph;
+	public Graph() {
+		graph = new ArrayList();		
+	}
+	
+	public void printAllConnections() {
+		for(int i=0;i<5;i++) {
+			System.out.println("\nPrinting connections of "+i);
+			for(int j=0;j<graph.get(i).size();j++) {
+				System.out.print(" "+ graph.get(i).get(j));
+			}
+		}
+	}
+
+	public void insertElement(int val) {
+		graph.add(new ArrayList());
+	}
+	
+	public void insertEdge(int u, int v) {
+		graph.get(u).add(v);
+		graph.get(v).add(u);
+	}
+	
+}
